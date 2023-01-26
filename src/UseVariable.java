d2731475dbf19f63f5372d6b4b7ccf9c7ce2ec55
takeo828 <takeo.i828@gmail.com>
2023-01-27 01:22:29 +0900

    Javaプログラミング入門学習終了

---
+++ src/UseVariable.java
@@ -0,0 +1,23 @@
+
+public class UseVariable {
+
+    public static void main(String[] args) {
+        int number1;
+        String str1;
+        number1 = 1;
+        str1 = "abc";
+        System.out.println(number1);
+        System.out.println(str1);
+
+        int number2 = 100;
+        String str2 = "xyz";
+        System.out.println(number2);
+        System.out.println(str2);
+
+     // 1日の秒数 = 24時間 x 60分 x 60秒
+        int secondsPerDay = 24 * 60 * 60;    // * は 掛け算を意味する記号
+
+        System.out.println(secondsPerDay);
+    }
+
+}
